694e1a6984359592193ba4974129ff38a818a7e0
Margarita Grushovets <ritagrushovets2002@gmail.com>
2024-01-25 03:21:10 +0200

    Design menu links

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ document.addEventListener("DOMContentLoaded", function() {
       container.appendChild(item);
     }
   });
+
+  var currentActiveLink = null;
+
+        // Set the home link as initially active
+        window.onload = function () {
+            var homeLink = document.getElementById('home');
+            updateText(homeLink);
+        };
+
+        function updateText(link) {
+            // If there's a previously clicked link, restore its text
+            if (currentActiveLink) {
+                currentActiveLink.textContent = currentActiveLink.dataset.originalText;
+            }
+
+            // Store the original text content in a data attribute
+            link.dataset.originalText = link.textContent;
+
+            // Create a span element with a class of "dot"
+            var dotElement = document.createElement('span');
+            dotElement.className = 'dot';
+
+            // Replace the link text with the created dot element
+            link.textContent = '';
+            link.appendChild(dotElement);
+
+            // Update the currentActiveLink to the currently clicked link
+            currentActiveLink = link;
+        }
